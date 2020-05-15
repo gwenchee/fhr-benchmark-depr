8371ff259da96f27b87e0944082404a9bb5373c5
gwenchee <gwendolynchee95@gmail.com>
2020-05-15 18:54:58 +0800

    graphite analysis

--- phase1a/case1a/case1a_analysis.py
+++ phase1a/case1a/case1a_analysis.py
@@ -28,6 +28,8 @@ beta_b(sp,case)
 print(reactivity_coefficient_b(keff_og=keff,keff_og_unc=keff_unc,keff_new=1.40445,keff_new_unc=0.00003,temp_change=+50))
 # flibe 
 print(reactivity_coefficient_b(keff_og=keff,keff_og_unc=keff_unc,keff_new=1.40745,keff_new_unc=0.00003,temp_change=+50))
+# graphite 
+print(reactivity_coefficient_b(keff_og=keff,keff_og_unc=keff_unc,keff_new=1.40674,keff_new_unc=0.00003,temp_change=+50))
 fission_density_c(sp,case)
 neutron_flux_d(sp,keff,keff_unc,case)
 neutron_flux_e(sp,keff,case)
