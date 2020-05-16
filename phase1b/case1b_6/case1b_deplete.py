f2af210e3f9c0ad7cfccdea61b0edcd7643d8768
gwenchee <gwendolynchee95@gmail.com>
2020-05-16 16:16:47 +0800

    128 nodes

---
+++ phase1b/case1b_6/case1b_deplete.py
@@ -0,0 +1,25 @@
+"""
+This python script runs the depletion calculation for case1b of the FHR 
+benchmark. 
+
+"""
+
+###############################################################################
+#                      Python Package Import
+###############################################################################
+import mpi4py.MPI
+import openmc.deplete
+from case1a_build_xml import * 
+
+###############################################################################
+#                                  Run
+###############################################################################
+
+chain = openmc.deplete.Chain.from_xml("../../data/chain_endfb71_pwr.xml")
+
+operator = openmc.deplete.Operator(geom, settings, "../../data/chain_endfb71_pwr.xml")
+
+time_steps = list(dep_time.copy() * 24 * 60 * 60)
+
+integrator = openmc.deplete.PredictorIntegrator(operator, time_steps, power_GW * 1e9)
+integrator.integrate()
