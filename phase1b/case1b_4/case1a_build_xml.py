69aebd7ea2e4c260f8ecaad83376279621aac58b
gwenchee <gwendolynchee95@gmail.com>
2020-05-16 15:29:15 +0800

    1000 particles

--- phase1b/case1b_4/case1a_build_xml.py
+++ phase1b/case1b_4/case1a_build_xml.py
@@ -23,7 +23,7 @@ from tallies import *
 # OpenMC simulation parameters
 batches = 100
 inactive = 10
-particles = 100
+particles = 1000
 tallies_on = True
 
 ###############################################################################
