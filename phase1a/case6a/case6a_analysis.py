a246499b118d1cc644b37e4f3164905dd59e8b17
gwenchee <gwendolynchee95@gmail.com>
2020-05-22 00:25:30 +0800

    case6a

--- phase1a/case6a/case6a_analysis.py
+++ phase1a/case6a/case6a_analysis.py
@@ -26,6 +26,8 @@ sp = openmc.StatePoint('h5files/3pcm/statepoint.500.h5')
 beta_b(sp,case)
 # doppler 
 print(reactivity_coefficient_b(keff_og=keff,keff_og_unc=keff_unc,keff_new=1.25084,keff_new_unc=0.00004,temp_change=+50))
+# flibe 
+print(reactivity_coefficient_b(keff_og=keff,keff_og_unc=keff_unc,keff_new=1.25489,keff_new_unc=0.00003,temp_change=+50))
 fission_density_c(sp,case)
 neutron_flux_d(sp,keff,keff_unc,case)
 neutron_flux_e(sp,keff,case)
