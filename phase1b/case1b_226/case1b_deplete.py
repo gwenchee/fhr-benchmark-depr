5efc1220d5b6f62e705133a5281f9887a2726fc0
gwenchee <gwendolynchee95@gmail.com>
2020-05-26 21:36:08 +0800

    depl

---
+++ phase1b/case1b_226/case1b_deplete.py
@@ -0,0 +1,28 @@
+"""
+This python script runs the depletion calculation for case1b of the FHR
+benchmark.
+
+"""
+
+###############################################################################
+#                      Python Package Import
+###############################################################################
+import openmc.deplete
+from case1a_build_xml import *
+
+###############################################################################
+#                                  Run
+###############################################################################
+
+chain = openmc.deplete.Chain.from_xml("../../data/chain_endfb71_pwr.xml")
+
+operator = openmc.deplete.Operator(geom, settings, "../../data/chain_endfb71_pwr.xml")
+
+time_steps = list(dep_time.copy() * 24 * 60 * 60)
+hmop = operator.heavy_metal
+print('hm = ' + str(hmop))
+
+integrator = openmc.deplete.CELIIntegrator(operator=operator, timesteps=bu, 
+                                           timestep_units='Mwd/kg',power=power_GW * 1e9)
+integrator.integrate()
+
