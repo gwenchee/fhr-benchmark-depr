ff62779a8653ab172f93b0fe1a2c1f646044f9f8
gwenchee <gwendolynchee95@gmail.com>
2020-05-27 20:44:37 +0800

    case 1b

--- phase1b/case1b_clean/case1b_deplete.py
+++ phase1b/case1b_clean/case1b_deplete.py
@@ -13,17 +13,17 @@ from case1a_build_xml import *
 ###############################################################################
 #                                  Run
 ###############################################################################
+chain_file = "../../data/chain_endfb71_pwr.xml"
+chain = openmc.deplete.Chain.from_xml(chain_file)
 
-chain = openmc.deplete.Chain.from_xml("../../data/chain_endfb71_pwr.xml")
-
-operator = openmc.deplete.Operator(geom, settings, "../../data/chain_endfb71_pwr.xml")
+operator = openmc.deplete.Operator(geom, settings, chain_file)
 
 
 
-time_steps = list(dep_time.copy() * 24 * 60 * 60)
+#time_steps = list(dep_time.copy() * 24 * 60 * 60)
 hmop = operator.heavy_metal
 print('hm = ' + str(hmop))
 
-integrator = openmc.deplete.CELIIntegrator(operator=operator, timesteps=time_steps, power_density=200)
+integrator = openmc.deplete.CELIIntegrator(operator=operator, timesteps=bu, timestep_units='Mwd/kg', power=power_GW*e9)
 integrator.integrate()
 
