87d476373116a883b572084aa5aad55a86dc4910
Gwendolyn JY Chee <gwendolynchee95@gmail.com>
2020-05-28 08:46:57 -0500

    1b

--- phase1b/case1b_clean/case1a_build_xml.py
+++ phase1b/case1b_clean/case1a_build_xml.py
@@ -23,7 +23,7 @@ from tallies import *
 # OpenMC simulation parameters
 batches = 10
 inactive = 1
-particles = 1000
+particles = 100
 tallies_on = False
 
 ###############################################################################
